d37586ad5db54735f9fd87dd8a7e1a8fe1820b86
Sroÿce <63264339+Fituning@users.noreply.github.com>
2024-06-03 14:13:56 +0200

    add fonts

--- app/src/main/java/com/utbm/sy43/f_one_companion/ui/theme/Type.kt
+++ app/src/main/java/com/utbm/sy43/f_one_companion/ui/theme/Type.kt
@@ -2,33 +2,43 @@ package com.utbm.sy43.f_one_companion.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.utbm.sy43.f_one_companion.R
 
 // Set of Material typography styles to start with
+val Formula1 = FontFamily(
+    Font(R.font.formula1_wide, FontWeight.ExtraBold),
+    Font(R.font.formula1_regular),
+    Font(R.font.formula1_bold, FontWeight.Bold),
+)
+
+val  Roboto = FontFamily(
+    Font(R.font.roboto_regular),
+    Font(R.font.roboto_bold, FontWeight.Bold)
+)
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+    displayLarge = TextStyle(
+        fontFamily = Formula1,
+        fontWeight = FontWeight.ExtraBold,
+        fontSize = 36.sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = Formula1,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontFamily = Formula1,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = Roboto,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp
     )
-    */
 )
