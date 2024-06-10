9959ce8a47e3006b6c2f4569809764e70c3a8c20
Sroÿce <63264339+Fituning@users.noreply.github.com>
2024-06-10 20:01:44 +0200

    connection app to firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -86,4 +87,15 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+
+    //importer les dépendances de navigation
+    implementation( "androidx.navigation:navigation-compose:2.4.0-alpha10")
+
+
+    // Import Firebase SDK
+    implementation(platform("com.google.firebase:firebase-bom:33.1.0"))
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-auth:21.0.1")
+    implementation("com.google.firebase:firebase-firestore:24.0.0")
 }
--- app/src/main/java/com/utbm/sy43/f_one_companion/activities/HomeActivity.kt
+++ app/src/main/java/com/utbm/sy43/f_one_companion/activities/HomeActivity.kt
@@ -17,7 +17,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Login
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -111,7 +111,7 @@ fun TopAppBar(
                 //modifier = Modifier . weight (1f)
             ) {
                 Icon(
-                    Icons.Default.Login,
+                    Icons.Default.AccountCircle,
                     contentDescription = "",
                 )
             }
