d724a70f308baf5c6b6d0de9b8a1878897728ffb
Sroÿce <63264339+Fituning@users.noreply.github.com>
2024-06-10 20:17:12 +0200

    create NavGraph.kt

---
+++ app/src/main/java/com/utbm/sy43/f_one_companion/NavGraph.kt
@@ -0,0 +1,15 @@
+package com.utbm.sy43.f_one_companion
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun NavGraph(startDestination: String = "login") {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = startDestination) {
+
+    }
+}
